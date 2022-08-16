3fc9c0842236d5bf006432cfc74e3e230e5c7cba
JonnnyP <jonathan_plata@ymail.com>
2022-08-16 17:58:57 -0400

    some basic postgres routes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./queries.js');
+
 const app = express();
 const port = 3000;
 
@@ -14,6 +16,11 @@ app.use(
 app.get('/', (request, response) => {
 	response.json({info: 'Node.js, express and Postgres API'});
 })
+
+app.get('/users', db.getAllUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.listen(port, () => {
 	console.log(`App running on port ${port}`);
